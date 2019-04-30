64fb0e0096c0c68e5d4e053226dcdfef9e22ebff
Rubén García Serrano <onitaku@gmail.com>
2019-04-30 14:44:07 +0200

    refactor, structure, logic

--- src/individualTable.js
+++ src/individualTable.js
@@ -1,267 +1,371 @@
 import React from "react";
-// import React, { Fragment } from "react";
-import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-import { createStore, combineReducers } from "redux";
-// import { applyMiddleware, createStore, combineReducers } from "redux";
-// import thunk from "redux-thunk";
-// import { logger } from "redux-logger";
+import { Provider as StoreProvider, connect } from "react-redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { logger } from "redux-logger";
 
-// import { TableBody, TableRow, TableCell } from "@material-ui/core";
 
 import individualList from "./individualsList";
 
 /* reducers*/
-function table(state = [], action) {
-    if (action.type === "ADD_INDIVIDUAL") {
-        return state.concat([action.obj]);
-    } else if (action.type === "DELETE_INDIVIDUAL") {
-        var index = state.indexOf(action.obj);
-        state.splice(index, 1);
-        return state;
-    } else if (action.type === "UPDATE_INDIVIDUAL") {
-        return state.map((todo, index, arr) => {
-            if (arr[index].id.toString() === action.obj.id) {
-                let obj = {};
-                obj[action.obj.name] = action.obj.value;
-                return Object.assign({}, todo, obj);
-            }
-            return todo;
-        });
-    } else {
-        return state;
-    }
-}
-function filter(state = "", action) {
-    if (action.type === "FILTER_TEXT") {
-        return action.text;
-    } else {
-        return state;
+function tableReducer(state = individualList, action) {
+    switch (!!action && action.type) {
+        case "ADD_INDIVIDUAL":
+            return state.concat([action.obj]);
+        case "DELETE_INDIVIDUAL":
+            return state.filter(
+                individual => individual.id !== action.individualId
+            );
+        case "DISCARD_INDIVIDUAL_CHANGES":
+            return state.map(individual => {
+                if (action.individualId === individual.id) {
+                    const { mod, ...otherProps } = individual;
+                    return otherProps;
+                } else {
+                    return individual;
+                }
+            });                
+        case "UPDATE_INDIVIDUAL_PROPERTY":
+            return state.map(individual => (
+                action.individualId !== individual.id ? individual : ({
+                    ...individual,
+                    mod: {
+                        ...individual.mod,
+                        [action.property]: action.value
+                    }
+                })
+            ));        
+        case "UPDATE_INDIVIDUAL":
+            return state.map((todo, index, arr) => {
+                if (arr[index].id.toString() === action.obj.id) {
+                    let obj = {};
+                    obj[action.obj.name] = action.obj.value;
+                    return Object.assign({}, todo, obj);
+                }
+                return todo;
+            });
+        default:
+            return state;
     }
 }
 
-var initialState = {
-    filter: "",
-    table: individualList
-};
+function filterReducer(state = "", action) {
+    switch (!!action && action.type) {
+        case "FILTER_TEXT":
+            return action.text;
+        default:
+            return state;
+    }
+}
 
-let reducer = combineReducers({ table, filter });
-let store = createStore(reducer, initialState);
+const reducer = combineReducers({
+    table: tableReducer,
+    filter: filterReducer
+});
+
+const initialState = reducer();
+
+const store = createStore(reducer, initialState, applyMiddleware(logger));
 
 class Individuals extends React.Component {
     constructor(props) {
         super(props);
     }
-
-    componentDidMount() {}
-    componentDidUpdate() {}
-    componentWillUnmount() {}
-
     render() {
         return (
             <div>
-                <SearchBar filterText={this.props.value.filter} />
-                <IndividualTable
-                    individuals={this.props.value.table}
-                    filterText={this.props.value.filter}
-                />
+                <ConnSearchBar />
+                <ConnIndividualTable />
             </div>
         );
     }
 }
-function SearchBar(props) {
-    return (
-        <div>
-            <input
-                type="text"
-                placeholder="Search..."
-                value={props.filterText}
-                onChange={evt => {
-                    store.dispatch({
-                        type: "FILTER_TEXT",
-                        text: evt.target.value
-                    });
-                }}
-            />
-        </div>
-    );
-}
 
-function IndividualTable(props) {
-    var filterText = props.filterText;
-    var individual = props.individuals.map(function(individual) {
-        if (individual.name.indexOf(filterText) === -1) {
-            return;
-        }
-        return <IndividualRow individual={individual} key={individual.id} />;
-    });
+Individuals.propTypes = {
+    value: PropTypes.object.isRequired
+};
+
+const SearchBar = props => (
+    <div>
+        <input
+            type="text"
+            placeholder="Search..."
+            value={props.filterText}
+            onChange={evt => {
+                props.onFilterTextChange(evt.target.value);
+            }}
+        />
+    </div>
+);
+
+SearchBar.propTypes = {
+    filterText: PropTypes.string,
+    onFilterTextChange: PropTypes.func.isRequired
+};
+
+const sbMapStateToProps = state => ({
+    filterText: state.filter
+});
+
+const sbMapDispatchToProps = dispatch => ({
+    onFilterTextChange: filterText => {
+        dispatch({
+            type: "FILTER_TEXT",
+            text: filterText
+        });
+    }
+});
+
+const ConnSearchBar = connect(
+    sbMapStateToProps,
+    sbMapDispatchToProps
+)(SearchBar);
+
+//==============================================================================
+// IndividualTable
+//==============================================================================
+
+const IndividualTable = ({
+    filterText,
+    individuals,
+    addIndividual,
+    ...props
+}) => {
+    const filteredIndividuals = !filterText
+        ? individuals
+        : individuals.filter(
+              individual => individual.name.indexOf(filterText) !== -1
+          );
+    const onAddIndividual = () => {
+        const id = (+new Date() + Math.floor(Math.random() * 9999)).toString(
+            36
+        );
+        addIndividual(id);
+    };
     return (
         <div>
             <button
                 type="button"
-                onClick={() =>
-                    store.dispatch({
-                        type: "ADD_INDIVIDUAL",
-                        obj: {
-                            category: "",
-                            id: (
-                                +new Date() + Math.floor(Math.random() * 999999)
-                            ).toString(36),
-                            name: "",
-                            price: "",
-                            qty: 0
-                        }
-                    })
-                }
+                onClick={onAddIndividual}
                 className="btn btn-success pull-right"
-            >
-                Add
-            </button>
+            >Add</button>
             <table className="table table-bordered">
                 <thead>
                     <tr>
                         <th>Name</th>
                         <th>ID</th>
                         <th>Sex</th>
-                        <th>karyotypicSex</th>
-                        <th>ethnicity</th>
-                        <th>population</th>
-                        <th>dateOfBirth</th>
-                        <th>lifeStatus</th>
-                        <th>affectationStatus</th>
+                        <th>Karyotypic Sex</th>
+                        <th>Ethnicity</th>
+                        <th>Population</th>
+                        <th>Date Of Birth</th>
+                        <th>Life Status</th>
+                        <th>Affectation Status</th>
                     </tr>
                 </thead>
-
-                <tbody>{individual}</tbody>
+                <tbody>
+                    {filteredIndividuals.map(individual => (
+                        <ConnIndividualRow
+                            individual={individual}
+                            key={individual.id}
+                        />
+                    ))}
+                </tbody>
             </table>
         </div>
     );
-}
+};
 
-function IndividualRow(props) {
-    return (
-        <tr className="eachRow">
-            <EditableCell
-                cellData={{
-                    type: "name",
-                    value: props.individual.name,
-                    id: props.individual.id
-                }}
-            />
-            <EditableCell
-                cellData={{
-                    type: "id",
-                    value: props.individual.id,
-                    id: props.individual.id
-                }}
-            />
-            <EditableCell
-                cellData={{
-                    type: "sex",
-                    value: props.individual.sex,
-                    id: props.individual.id
-                }}
-            />
-            <EditableCell
-                cellData={{
-                    type: "karyotypicSex",
-                    value: props.individual.karyotypicSex,
-                    id: props.individual.id
-                }}
-            />
-            <EditableCell
-                cellData={{
-                    type: "ethnicity",
-                    value: props.individual.ethnicity,
-                    id: props.individual.id
-                }}
-            />
-            <EditableCell
-                cellData={{
-                    type: "population",
-                    value: props.individual.population.name,
-                    id: props.individual.id
-                }}
-            />
-            <EditableCell
-                cellData={{
-                    type: "dateOfBirth",
-                    value: props.individual.dateOfBirth,
-                    id: props.individual.id
-                }}
-            />
-            <EditableCell
-                cellData={{
-                    type: "lifeStatus",
-                    value: props.individual.lifeStatus,
-                    id: props.individual.id
-                }}
-            />
-            <EditableCell
-                cellData={{
-                    type: "affectationStatus",
-                    value: props.individual.affectationStatus,
-                    id: props.individual.id
-                }}
-            />
-            <td className="del-cell">
-                <input
-                    type="button"
-                    onClick={() =>
-                        store.dispatch({
-                            type: "DELETE_INDIVIDUAL",
-                            obj: props.individual
-                        })
-                    }
-                    value="X"
-                    className="del-btn"
-                />
-            </td>
-            <td className="apply-row">
-                <input
-                    type="button"
-                    onClick={() =>
-                        store.dispatch({
-                            type: "UPDATE_INDIVIDUAL",
-                            obj: props.individual
-                        })
-                    }
-                    value="V"
-                    className="del-btn"
-                />
-            </td>
-        </tr>
-    );
-}
-function EditableCell(props) {
-    return (
-        <td>
+IndividualTable.propTypes = {
+    filterText: PropTypes.string,
+    individuals: PropTypes.array.isRequired,
+    addIndividual: PropTypes.func.isRequired
+};
+
+const itMapStateToProps = state => ({
+    filterText: state.filter,
+    individuals: state.table
+});
+
+const itMapDispatchToProps = dispatch => ({
+    addIndividual: id => {
+        dispatch({
+            type: "ADD_INDIVIDUAL",
+            obj: {
+                id: id,
+                name: "",
+            }
+        });
+    }
+});
+
+const ConnIndividualTable = connect(
+    itMapStateToProps,
+    itMapDispatchToProps
+)(IndividualTable);
+
+
+const getPopulationName = individual => {
+    
+    const props = { individual };
+    if (!!individual.mod) {
+    }
+
+    return (!!props.individual.mod &&
+        props.individual.mod.population) ||
+    (!!props.individual.population &&
+        props.individual.population.name) ||
+    "";
+
+};
+
+const IndividualRow = props => (
+    <tr className="eachRow">
+        <EditableCell
+            individualId={props.individual.id}
+            propertyId={"name"}
+            propertyValue={props.individual.name}
+        />
+        <EditableCell
+            individualId={props.individual.id}
+            propertyId={"id"}
+            propertyValue={props.individual.id}
+        />
+        <EditableCell
+            individualId={props.individual.id}
+            propertyId={"sex"}
+            propertyValue={props.individual.sex}
+        />
+        <EditableCell
+            individualId={props.individual.id}
+            propertyId={"karyotypicSex"}
+            propertyValue={props.individual.karyotypicSex}
+        />
+        <EditableCell
+            individualId={props.individual.id}
+            propertyId={"ethnicity"}
+            propertyValue={props.individual.ethnicity}
+        />
+        <EditableCell
+            individualId={props.individual.id}
+            propertyId={"population"}
+            propertyValue={getPopulationName(props.individual)}
+        />
+        <EditableCell
+            individualId={props.individual.id}
+            propertyId={"dateOfBirth"}
+            propertyValue={props.individual.dateOfBirth}
+        />
+        <EditableCell
+            individualId={props.individual.id}
+            propertyId={"lifeStatus"}
+            propertyValue={props.individual.lifeStatus}
+        />
+        <EditableCell
+            individualId={props.individual.id}
+            propertyId={"affectationStatus"}
+            propertyValue={props.individual.affectationStatus}
+        />
+        <td className="del-cell">
             <input
-                type="text"
-                id={props.cellData.id}
-                value={props.cellData.value}
-                name={props.cellData.type}
-                onChange={evt => {
-                    store.dispatch({
-                        type: "UPDATE_INDIVIDUAL",
-                        obj: {
-                            id: evt.target.id,
-                            name: evt.target.name,
-                            value: evt.target.value
-                        }
-                    });
-                }}
+                type="button"
+                onClick={props.removeIndividual}
+                value="X"
+                className="del-btn"
             />
         </td>
-    );
-}
+        <td className="apply-row">
+            <input
+                type="button"
+                onClick={props.updateIndividual}
+                value="✓"
+                className="del-btn"
+            />
+        </td>
+        <td className="apply-row">
+            <input
+                type="button"
+                onClick={props.discardChanges}
+                value="R"
+                className="del-btn"
+            />
+        </td>
+    </tr>
+);
 
-const render = () =>
-    ReactDOM.render(
-        <Individuals value={store.getState()} />,
-        document.getElementById("root")
-    );
-render();
-store.subscribe(render);
+const mapStateToProps = state => ({});
 
-export default IndividualTable;
+const mapDispatchToProps = (dispatch, ownProps) => ({
+    updateIndividual: () => {
+        dispatch({
+            type: "UPDATE_INDIVIDUAL",
+            obj: ownProps.individual
+        });
+    },
+    removeIndividual: () => {
+        dispatch({
+            type: "DELETE_INDIVIDUAL",
+            individualId: ownProps.individual.id
+        });
+    },
+    discardChanges: () => {
+        dispatch({
+            type: "DISCARD_INDIVIDUAL_CHANGES",
+            individualId: ownProps.individual.id,
+        });
+    },
+});
+
+const ConnIndividualRow = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(IndividualRow);
+
+const EditableCellView = ({
+    propertyValue,
+    updateIndividualProperty
+}) => (
+    <td>
+        <input
+            type="text"
+            value={propertyValue}
+            onChange={updateIndividualProperty}
+        />
+    </td>
+);
+
+EditableCellView.propTypes = {
+    propertyId: PropTypes.string.isRequired,
+    propertyValue: PropTypes.string.isRequired
+};
+
+EditableCellView.defaultProps = {
+    propertyValue: ""
+};
+
+const ecMapStateToProps = state => ({});
+
+const ecMapDispatchToProps = (dispatch, ownProps) => ({
+    updateIndividualProperty: event => {
+        dispatch({
+            type: "UPDATE_INDIVIDUAL_PROPERTY",
+            individualId: ownProps.individualId,
+            property: ownProps.propertyId,
+            value: event.target.value
+        });
+    }
+});
+
+const EditableCell = connect(
+    ecMapStateToProps,
+    ecMapDispatchToProps
+)(EditableCellView);
+
+const RootElement = () => (
+    <StoreProvider store={store}>
+        <Individuals value={store.getState()} />
+    </StoreProvider>
+);
+
+export default RootElement;
